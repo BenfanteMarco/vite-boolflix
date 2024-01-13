68f190c33c021736739505ed1b077dc3031db363
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-01-13 17:51:10 +0100

    added movieApi to appvue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script>
+import { store } from './store';
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 export default {
@@ -11,10 +13,21 @@ export default {
 
     }
   },
+  methods: {
+    getMovies(){
+      let searchMovies = store.movieApi;
+      if(store.search !== ''){
+        searchMovies += `&query=${store.search}`
+      }
+      axios.get(searchMovies).then((response) => {
+        this.store.movies = response.data.results
+      });
+    }
+  }
 }
 </script>
 <template lang="">
-  <AppHeader />
+  <AppHeader @filterMovie="getMovies()" />
   <AppMain />
 </template>
 <style lang="scss">
